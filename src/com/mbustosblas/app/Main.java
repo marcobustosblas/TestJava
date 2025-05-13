cea108c081fca3f028dc585aa34355542f39cf86
Marco <114123687+marcobustosblas@users.noreply.github.com>
2025-05-13 17:15:14 -0400

    Initial commit in local

---
+++ src/com/mbustosblas/app/Main.java
@@ -0,0 +1,12 @@
+package com.mbustosblas.app;
+import com.mbustosblas.app.hogar.Persona;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Persona p = new Persona();
+        p.name = "Marco";
+        System.out.println(p.name);
+
+    }
+}
